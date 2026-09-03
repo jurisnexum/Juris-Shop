70016f45937875dd532c2e8915c69197a72c1c82
jurisnexum <yawitch8@gmail.com>
2026-09-03 12:03:40 +0000

    Connect shop to Google Apps Script

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
-// Paste the deployed Google Apps Script Web App URL here.
-const API_URL = "PASTE_YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE";
+// Google Apps Script Web App URL
+const API_URL = "https://script.google.com/macros/s/AKfycbz7jE-iFJFOep9n5vx-t9yjNOw6rTGCwsx-ds6HQNCGZ_TW0mtOTC9OG8qzw4z8C6Se/exec";
